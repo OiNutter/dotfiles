cdd82fdddc355c951b3dfe9454acc0dd5b1b591e
OiNutter <willmckenzie@oinutter.co.uk>
2012-04-26 23:38:06 +0100

    Added python module template

---
+++ skeletor/templates/python-module/includes/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='<skeleton_project>',
+	  version='0.1',
+	  description='ADD DESCRIPTION HERE',
+	  author='Will McKenzie',
+	  author_email='will@oinutter.co.uk',
+	  packages=['<skeleton_project>'],
+	  package_dir={'<skeleton_project>': '<skeleton_project'},
+      package_data={},
+      requires:[]
+	  )
