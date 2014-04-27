3286dfced8cc8dd8260c21ee5ca9493f308e64fb
OiNutter <willmckenzie@oinutter.co.uk>
2014-04-28 00:37:09 +0100

    oh-my-zsh setup

---
+++ zsh/install.sh
@@ -0,0 +1,5 @@
+# install oh-my-zsh and symlink my theme
+
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
+ln -s $HOME/.dotfiles/zsh/oinutter.zsh-theme $HOME/.oh-my-zsh/themes/oinutter.zsh-theme
