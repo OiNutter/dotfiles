829f71605e15571e8d05ef843c97f1d06a0644eb
OiNutter <willmckenzie@oinutter.co.uk>
2014-04-28 00:43:47 +0100

    Remove silent logging of homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,10 +9,10 @@
 if test ! $(which brew)
 then
 echo " Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
 # Install homebrew packages
-brew install rbenv ruby-build pyenv phantomjs ant bats ctags mercurial mongodb openssl phantomjs pow readline wget git 
+brew install rbenv ruby-build pyenv phantomjs ant bats ctags mercurial mongodb openssl phantomjs pow readline wget git
 
 exit 0
